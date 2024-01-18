e948be6e5833bf8d3aeaf3bba081098aa6220dac
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2024-01-18 15:25:10 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-echo "Installing Frontend dependencies"
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo "${CYAN} ===> Installing Frontend dependencies${NC}"
 yarn
-echo "Installing backend dependencies"
+echo "${CYAN} ===> Installing backend dependencies${NC}"
 cd backend && cp .env.example .env && npm i
 cd ..
 yarn add -D concurrently
